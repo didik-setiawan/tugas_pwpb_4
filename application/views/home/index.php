5e7ba3dc47431c02547219b94111669a845401fb
didik-setiawan <didiksetyaone0@gmail.com>
2020-04-14 16:16:52 +0700

    Tugas ke 4

---
+++ application/views/home/index.php
@@ -0,0 +1,184 @@
+<div class="jumbotron jumbotron-fluid">
+     <div class="container text-center">
+         <img src="<?= base_url(); ?>asset/img/download.jpg" width="25%"  class="rounded-circle img-thumbnail">
+         <h1 class="display-4">DIDIK SETIAWAN</h1>
+         <p class="lead">WELLCOME TO JALAN SURGA NO 201</p>
+     </div>
+
+ </div>
+
+ <div class="container">
+    <div class="row mb-4">
+      <div class="col text-center">
+        <h2>About</h2>
+      </div>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-md-5 text-center">
+        <p class="ki"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Illo hic repellat qui suscipit velit. Similique autem aliquid
+           facere in nisi consequuntur explicabo obcaecati accusamus maiores 
+           beatae nostrum, quisquam, sed reprehenderit?</p>
+      </div>
+      <div class="col-md-5 text-center">
+        <p class="ka">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+           Facilis deleniti autem, alias, sequi quisquam modi vel facere labore 
+           fuga non incidunt? Nam, esse ipsum? Assumenda at aut deleniti natus asperiores?</p>
+      </div>
+    </div>
+ </div>
+ <section id="portfolio" class="portfolio bg-light">
+ <div class="container">
+   <div class="row mb-4 pt-4">
+     <div class="col text-center">
+       <h2>PORTOFOLIO</h2>
+     </div>
+   </div>
+   <div class="row">
+     <div class="col-sm-4">
+        <div class="thumbnail">
+         <div class="card">
+            
+          <img class="card-img-top " src="<?= base_url(); ?>asset/img/capture.png" alt="card image cap">
+          <div class="card-body">
+            <p class="card-text"> marsmallow asli indonesia naik mator plat p</p>
+          </div>
+        </div>
+      </div>
+     </div>
+     <div class="col-md">
+        <div class="thumbnail">
+      <div class="card">
+         
+       <img class="card-img-top " src="<?= base_url(); ?>asset/img/acer.jpg" alt="card image cap">
+       <div class="card-body">
+         <p class="card-text">laptop gaming termahal di dunia spek dewa</p>
+       </div>
+     </div>
+      </div>
+    </div>
+  
+    <div class="col-md">
+        <div class="thumbnail">
+    <div class="card">
+        
+     <img class="card-img-top " src="<?= base_url(); ?>asset/img/cs.jpg" alt="card image cap">
+     <div class="card-body">
+       <p class="card-text">counter strike online resmi tutup</p>
+     </div>
+        </div>
+   </div>
+</div>
+   </div>
+   <div class="row mb-4">
+    <div class="col-md">
+        <div class="thumbnail">
+        <div class="card">
+           
+         <img class="card-img-top " src="<?= base_url(); ?>asset/img/huawei.jpg" alt="card image cap">
+         <div class="card-body">
+           <p class="card-text">huawei perkenalkan handphone lipat</p>
+         </div>
+            </div>
+       </div>
+    </div>
+   
+    <div class="col-md">
+        <div class="thumbnail">
+     <div class="card">
+        
+      <img class="card-img-top " src="<?= base_url(); ?>asset/img/micr.png" alt="card image cap">
+      <div class="card-body">
+        <p class="card-text">lambang baru perusahaan microsoft</p>
+      </div>
+    </div>
+     </div>
+ </div>
+   <div class="col-md">
+      <div class="thumbnail">
+   <div class="card">
+      
+    <img class="card-img-top " src="<?= base_url(); ?>asset/img/rossi.jpg" alt="card image cap">
+    <div class="card-body">
+      <p class="card-text">rossi akui tak punya banyak waktu bersama yamaha</p>
+    </div>
+  </div>
+  </div>
+</div>
+  </div> 
+</div>
+</section>
+
+<div class="asd">
+<section id="contact" class="contact">
+<div class="container">
+  <div class="row pt-4 mb-4">
+    <div class="col text-center">
+      <h2>Contact Us</h2>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-lg-4">
+      <div class="p1">
+      <div class="card text-white bg-warning mb-3 text-center">
+      <div class="card-body">
+        <h5 class="card-title">Contact Us</h5>
+        <p class="card-text">silahkan kirim kritik dan saran anda di sini</p>
+      </div>
+      </div>
+    </div>
+      <ul class="list-group">
+          <li class="list-group-item"><h2>Location</h2></li>
+          <li class="list-group-item">My Office</li>
+          <li class="list-group-item">Jl Surga No 201 Block M</li>
+          <li class="list-group-item">Umbulsari,Jember,Jawa Timur,Indonesia</li>
+        </ul>
+      
+      </div>
+
+      <div class="col-lg-6">
+          <form>
+              <div class="form-group">
+                <label for="Nama">Nama</label>
+                <input type="text" class="form-control" id="Nama">
+              </div>
+              <div class="form-group">
+                  <label for="alamat">Alamat</label>
+                  <input type="text" class="form-control" id="alamat">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" id="email">
+                  </div>
+                  <div class="form-group">
+                      <label for="telp">No Tlp</label>
+                      <input type="text" class="form-control" id="tlp">
+                </div>
+                <div class="form-group">
+                    <label for="pesan">Pesan</label>
+                   <textarea name="pesan" id="pesan" class="form-control"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <button type="button" class="btn btn-primary"> kirim pesan</button>
+                  </div>
+            </form> 
+      </div>
+    </div>
+
+  </div>
+</div>
+</section>
+</div>
+
+<footer class="bg-dark text-white">
+  <div class="container">
+    <div class="row pt-3">
+      <div class="col text-center">
+        <p>copyright 2020</p>
+        <p>powered by DIDIK SETIAWAN </p>
+      </div>
+    </div>
+  </div>
+
+</footer>
---
+++ application/views/templates/footer.php
@@ -0,0 +1,9 @@
+
+<script scr="http://localhost/ci-app/asset/js/jquery-3.3.1.slim.min.js"></script>
+<script scr="http://localhost/ci-app/asset/js/bootstrap.js"></script>
+<script scr="http://localhost/ci-app/asset/js/popper.min.js"></script>
+<!-- <script src="http://localhost/ci-app/asset/js/sweetalert2.all.min.js"></script>
+<script src="http://localhost/ci-app/asset/js/myscript.js"></script> -->
+<!-- <script src="http://localhost/ci-app/asset/js/style.js"></script> -->
+</body>
+</html>
